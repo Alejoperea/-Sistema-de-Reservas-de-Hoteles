ba636186f691d012476da0003e249d3fd3f13447
Alejo Perea <mpereamejia@gmail.com>
2024-04-21 16:01:11 -0500

    Creacion Controladores y Index de hoteles

---
+++ resources/views/hotel/index.blade.php
@@ -0,0 +1,47 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Sistema de Reserva de Hoteles</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1 class="text-center">Sistema de Reserva de Hoteles</h1>
+      <a href="{{ route('hoteles.index') }}" class="btn btn-success">Agregar</a>
+      <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Ubicación</th>
+              <th scope="col">Número de Teléfono</th>
+              <th scope="col">Email de Contacto</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+              @foreach ($hoteles as $hotel)
+                <tr>
+                  <th scope="row">{{ $hotel->id }}</th>
+                  <td>{{ $hotel->nombre }}</td>
+                  <td>{{ $hotel->ubicacion }}</td>
+                  <td>{{ $hotel->numero_telefonico }}</td>
+                  <td>{{ $hotel->email_contacto }}</td>
+                  <td><span>Acciones</span></td>
+                </tr>
+              @endforeach
+          </tbody>
+        </table>
+    </div>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
